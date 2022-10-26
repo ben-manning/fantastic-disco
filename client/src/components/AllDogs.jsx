48e46c524ad2ec5598151afc5c3a1a4424b9bf85
Ben Manning <bmanning@me.com>
2022-10-26 11:15:02 -0400

    adding the logic for getting all dogs

--- client/src/components/AllDogs.jsx
+++ client/src/components/AllDogs.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 const AllDogs = () => {
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+    const getDogs = async () => {
+      let res = await axios.get('http://localhost:3001/dogs')
+
+      setDogs(res.data)
+    }
+
+    getDogs()
+  }, [])
+
   return (
     <div>
       <h1>These are all of the dogs</h1>
+      { dogs.map((dog) => (
+        <div key={dog._id}>
+          <h3>{ dog.name }</h3>
+        </div>
+      ))}
     </div>
   )
 }
