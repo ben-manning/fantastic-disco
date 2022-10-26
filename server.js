9afecd3641a1384686e28a95ab5f75e42f38240b
Ben Manning <bmanning@me.com>
2022-10-26 10:57:32 -0400

    adding dependencies for front end

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 const db = require('./db')
 const { Dog } = require('./models')
+
+// middleware
+app.use(cors())
+app.use(express.json())
 
 // get all dogs
 app.get('/dogs', async (req, res) => {
